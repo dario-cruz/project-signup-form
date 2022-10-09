4795dac77549c6f90809e403da7492a798ac4e3f
Dario Cruz <rio2012@gmail.com>
2022-10-09 08:01:50 -0400

    Add js code for password match

--- code.js
+++ code.js
@@ -1,33 +1,48 @@
 const passInput = document.getElementById('password')
-const passConf = document.getElementById('confpass')
+const passConf = document.getElementById('conf-password')
 const fName = document.getElementById('fname')
 const lName = document.getElementById('lname')
 const email = document.getElementById('email')
+const passMsg = document.getElementById('confmessage')
 
-function passValid() {
-    if (passConf === passInput) {
-        passConf.setCustomValidity('');
+// Check password inputs to confirm matching values.
+const pCheck = () => {
+    if (passInput.value === passConf.value) {
+        passMsg.style.color = "green"
+        passMsg.innerHTML = "Passwords Match!"
     } else {
-        passConf.setCustomValidity('Passwords do not match');
+        passMsg.style.color = "red"
+        passMsg.innerHTML = "Passwords need to match."
     }
 }
 
-passInput.addEventListener('onChange', () => {
-    passValid();
-})
 
-passConf.addEventListener('onChange', () => {
-    passValid();
-})
 
-passInput.addEventListener("invalid", () => {
-    if (passInput.value === "") {
-        passInput.setCustomValidity("Enter your password")
-    }
-});
 
-passConf.addEventListener("invalid", () => {
-    if (passConf.value === "") {
-        passConf.setCustomValidity("Enter your password again to confirm.")
-    }
-});
+// function passValid() {
+//     if (passConf === passInput) {
+//         passConf.setCustomValidity('');
+//     } else {
+//         passConf.setCustomValidity('Passwords do not match');
+//     }
+// }
+
+// passInput.addEventListener('onChange', () => {
+//     passValid();
+// })
+
+// passConf.addEventListener('onChange', () => {
+//     passValid();
+// })
+
+// passInput.addEventListener("invalid", () => {
+//     if (passInput.value === "") {
+//         passInput.setCustomValidity("Enter your password")
+//     }
+// });
+
+// passConf.addEventListener("invalid", () => {
+//     if (passConf.value === "") {
+//         passConf.setCustomValidity("Enter your password again to confirm.")
+//     }
+// });
